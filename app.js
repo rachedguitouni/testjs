7a157e240669b7e3dc77b78ca6c7d81ba3d186a3
rached <Rached.guitouni24@gmail.com>
2023-01-15 16:41:13 +0100

    else if

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ let num = prompt ("type a numbre bigger then 5")
 if (num>5){
     console.log("true");
 }
+else if (num == 5){
+    console.log("num is equal to 5")
+}
 else{
     console.log("please type a numbre that is bigger then 5");
 }
